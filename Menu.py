841eadce0cd5c3638cd24d91caaf60834134e162
Júlia Feitosa <julia.cfeitosa@hotmail.com>
2016-06-01 09:06:13 -0300

    Esqueleto do Menu do projeto

---
+++ Menu.py
@@ -0,0 +1,24 @@
+def StatusCliente():
+	print "StatusCliente"
+def Sair():
+	print "Sair"
+
+def Menu():
+
+	print "1. Status do Cliente"
+	print "2. Sair"
+	print "Escolha um"
+	Bla()
+	while True:
+		opt = raw_input()	
+		if(opt == "1"):
+			StatusCliente()
+			break
+		elif(opt == "2"):
+			Sair()
+			break
+		else:
+			print "Digite uma opcao valida!!!"
+
+
+Menu()
